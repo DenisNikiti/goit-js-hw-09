d388864160dbc2c90e6d324e9a387a483ad8cebf
DenisNikiti <den.hikitin@gmail.com>
2022-10-04 04:29:40 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,22 @@
 
 const startEl = document.querySelector("[data-start]")
 const stopEl = document.querySelector("[data-stop]")
+ let x 
 
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-let = x
+
 
 startEl.addEventListener("click", () => {
-     x = setTimeout(() => {
-        document.body.style.backgroundColor = getRandomHexColor()
-     }, 1000);
+   x = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor()
+   }, 1000);
+    
     stopEl.disabled = false
     startEl.disabled = true
+    
 })
 
 stopEl.addEventListener("click", () => { 
