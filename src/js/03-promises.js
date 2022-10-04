88e888ba560f11fa97a4d1b7443ab0af5d633235
DenisNikiti <den.hikitin@gmail.com>
2022-10-04 07:25:31 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,16 @@
 
  const formEl = document.querySelector(".form")
 
-function createPromise(position, delay) {
+function createPromise(amount, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+   resolve
   } else {
-    // Reject
+    
   }
 }
+
+
 
 
 formEl.addEventListener("submit",onformSubmit)
@@ -16,4 +18,32 @@ formEl.addEventListener("submit",onformSubmit)
 function onformSubmit(evt) {
   evt.preventDefault()
   const formdata = new FormData(evt.currentTarget)
+
+  let delay = 0
+  let step = 0
+  let amount = 0
+  
+       formdata.forEach((value, name) => {
+        if (name === "delay") {
+          Amount = value
+         }   
+         if (name === "step") {
+          step = value
+         }
+         if (name === "amount") {
+          amount = value
+         }
+  }) 
+ 
+ 
+createPromise(amount, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+
+
   }
