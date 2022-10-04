44e2347f1c76845e07618017755b2209ae9c7949
DenisNikiti <105548050+DenisNikiti@users.noreply.github.com>
2022-10-04 03:48:52 +0000

    Deploying to gh-pages from @ DenisNikiti/goit-js-hw-09@3e4731be9fea0f2ce6900324f303273f76ca5156 🚀

--- 03-promises.2c20e779.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelectorAll("[required]");console.log("asfsdfsdfdsf")}();
-//# sourceMappingURL=03-promises.2c20e779.js.map
--- 03-promises.50a2bad8.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll("[required]");console.log("asfsdfsdfdsf");
-//# sourceMappingURL=03-promises.50a2bad8.js.map
