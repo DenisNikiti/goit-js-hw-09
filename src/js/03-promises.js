6f55ce479664e56e814a15bd1101054c43aa4c65
DenisNikiti <den.hikitin@gmail.com>
2022-10-04 13:23:29 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,26 @@
 
  const formEl = document.querySelector(".form")
 
-const createPromise = new Promise((resolve, reject) =>{
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve()
-  } else {
-    reject()
+
+  
+
+function createPromise(amount, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve()
+    } else {
+      reject()
+    }
   }
-}
- )
+    )
+    
+  }
+
+
+  
+
+
 
 
 
@@ -17,34 +28,35 @@ const createPromise = new Promise((resolve, reject) =>{
 formEl.addEventListener("submit",onformSubmit)
 
 function onformSubmit(evt) {
+  
   evt.preventDefault()
   const formdata = new FormData(evt.currentTarget)
 
-  letdelay = 0
-  let step = 0
-  let amount = 0
+  let delay = 0;
+  let step = 0;
+  let amount = 0;
   
        formdata.forEach((value, name) => {
         if (name === "delay") {
-          delay = value
+          delay = value;
          }   
          if (name === "step") {
-          step = value
+           step = value;
          }
          if (name === "amount") {
-          amount = value
+           amount = value;
          }
   }) 
  
  
-
-createPromise
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+createPromise(amount,delay)
+  .then(({ amount, delay }) => {
+    console.log(`✅ Fulfilled promise ${amount} in ${delay}ms`);
   })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  .catch(({ amount, delay }) => {
+    console.log(`❌ Rejected promise ${amount} in ${delay}ms`);
   });
+
 
 
 
@@ -52,3 +64,5 @@ createPromise
 
 }
   
+
+
