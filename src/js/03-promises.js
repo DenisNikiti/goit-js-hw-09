d7c630b6ad7e8ad811ceb4b7f123f02d34f36a4e
DenisNikiti <den.hikitin@gmail.com>
2022-10-04 06:46:04 +0300

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,6 @@
+
+const inputs = document.querySelectorAll("[required]")
+console.log("asfsdfsdfdsf")
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
