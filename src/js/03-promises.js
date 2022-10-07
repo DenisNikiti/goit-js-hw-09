789909581e83065396040b930e969a2d10d86214
DenisNikiti <den.hikitin@gmail.com>
2022-10-07 15:58:11 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,31 +4,28 @@
 
   
 
-function createPromise(amount, delay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-   
-     setInterval(() => {
-      
-       
-        if (shouldResolve) {
-      resolve()
-    } else {
-      reject()
-    }
-       
-       
-       
-     }, 500);
-   
-   
-  }
-    )
+  
+function createPromise(delay) {
+  
+  return new Promise((resovle, reject) => {
+      const shouldResolve = Math.random() > 0.3;         
+            setTimeout(() => {
+                  if (shouldResolve) {
+                        
+                resovle(delay)
+             }
+               reject(delay)
+            }, delay);
     
+  })
+
+
+
   }
 
 
   
+
 
 
 
@@ -42,37 +39,54 @@ function onformSubmit(evt) {
   evt.preventDefault()
   const formdata = new FormData(evt.currentTarget)
 
-  let delay = 0;
-  let step = 0;
-  let amount = 0;
   
-       formdata.forEach((value, name) => {
-        if (name === "delay") {
-          delay = value;
-         }   
-         if (name === "step") {
-           step = value;
-         }
-         if (name === "amount") {
-           amount = value;
-         }
-  }) 
- 
- 
-createPromise(amount,delay)
-  .then(({ amount, delay }) => {
-    console.log(`✅ Fulfilled promise ${amount} in ${delay}ms`);
+    let delay = 0;
+  let step = 0;
+  let amount =0;
+
+
+  formdata.forEach((value, name) => {
+    if (name === "delay") {
+      delay = value;
+    }
+    if (name === "step") {
+      step = value;
+    }
+    if (name === "amount") {
+      amount = value;
+    }
+
+
   })
-  .catch(({ amount, delay }) => {
-    console.log(`❌ Rejected promise ${amount} in ${delay}ms`);
-  });
 
 
+  
+
+    for (let index = 1; index <= 5; index += 1) {
+      
+createPromise(delay)
+    .then((delay ) => {
+      console.log(`✅ Fulfilled promise ${index} in ${delay}ms`);
+        
+    })
+    .catch((delay ) => {
+      console.log(`❌ Rejected promise ${index} in ${delay}ms`);
+      
+    });
+
+      delay += step
+    }
 
 
-
+ 
+ 
+ 
 
 }
-  
 
 
+
+
+
+
+ 
