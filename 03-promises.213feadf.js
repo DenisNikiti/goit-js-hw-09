2fb37cf1d73276c3efb48c2cc42e9555bf1d83e7
DenisNikiti <105548050+DenisNikiti@users.noreply.github.com>
2022-10-08 00:47:58 +0000

    Deploying to gh-pages from @ DenisNikiti/goit-js-hw-09@3467f8717194546c51f93c24785f8d19931dcbca 🚀

--- 03-promises.213feadf.js
+++
@@ -1,2 +0,0 @@
-function e(e){return new Promise(((o,t)=>{const n=Math.random()>.3;setTimeout((()=>{n&&o(e),t(e)}),e)}))}document.querySelector(".form").addEventListener("submit",(function(o){o.preventDefault();const t=new FormData(o.currentTarget);let n=0,r=0,c=0;t.forEach(((e,o)=>{"delay"===o&&(n=e),"step"===o&&(r=e),"amount"===o&&(c=e)})),console.log(n,r,c);for(let o=1;o<=c;o+=1)e(n).then((e=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)})).catch((e=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)})),n+=r}));
-//# sourceMappingURL=03-promises.213feadf.js.map
